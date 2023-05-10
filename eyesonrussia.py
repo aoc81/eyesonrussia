65304f0e969afc5e4ea393c151598ad0403fbfd2
aoc81 <94049092+aoc81@users.noreply.github.com>
2023-05-10 22:31:19 +0200

    Update eyesonrussia.py

--- eyesonrussia.py
+++ eyesonrussia.py
@@ -1,8 +1,10 @@
+# This script made by aoc81 is based on the Eyes on Russia Map data, which is a project from The Centre for Information Resilience
+# For more info about the project, please visit https://eyesonrussia.org
+
 import pandas as pd
 import requests
 import json
 import time
-
 import argparse
 
 parser = argparse.ArgumentParser(description='Generates a JSON file with events data from a URL')
@@ -12,9 +14,6 @@ parser.add_argument('--country', type=str, help='Filter by country')
 parser.add_argument('--category', type=str, help='Filter by category')
 parser.add_argument('--latlon', type=str, help='Filter events by latitude and longitude (lat,lon)')
 parser.add_argument('--format', type=str, choices=['json', 'excel'], default='json', help='Output file format (json or excel)')
-
-
-
 args = parser.parse_args()
 
 
@@ -63,21 +62,18 @@ for feature in features[0:-1]:
     events.append(event_details)
     
 elapsed_time = time.time() - start_time
+
 print("[*] Retrieving and reformating data based on your inputs... ")
 
+#list comprehension to filter data based on given arguments
 if args.city:
     events = [e for e in events if e['city'] == args.city]
-
 if args.date:
     events = [e for e in events if e['date'] == args.date]
-
 if args.country:
     events = [e for e in events if e['country'] == args.country]
-
 if args.category:
     events = [e for e in events if any(args.category.lower() in c.lower() for c in e['categories'])]
-    
-
 if args.latlon:
     lat, lon = [round(float(coord.strip()), 1) for coord in args.latlon.split(',')]
     events = [e for e in events if abs(e['latitude'] - lat) <= 0.1 and abs(e['longitude'] - lon) <= 0.1]
@@ -88,7 +84,8 @@ if args.format == 'json':
     with open(file_name, 'w', encoding='utf-8') as file:
         json.dump(events, file, ensure_ascii=False, indent=4)
     print("[*] Operation has finished succesfully")
-    print("[*]", len(events), f'events deta have been added to the file "UWMaphub_events.json" in {str(elapsed_time)[0:4]} seconds')  
+    print("[*]", len(events), f'events deta have been added to the file "UWMaphub_events.json" in {str(elapsed_time)[0:4]} seconds')
+    
 elif args.format == 'excel':
     file_name = f"UWMaphub_events_filtered{'_' + args.city if args.city else ''}{'_' + args.date if args.date else ''}{'_' + args.country if args.country else ''}{'_' + args.category if args.category else ''}{'_' + args.latlon if args.latlon else ''}.xlsx"
     df = pd.DataFrame(events)
