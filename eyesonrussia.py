a666f9799667d5f71ba96865a8571f255d7b0877
aoc81 <94049092+aoc81@users.noreply.github.com>
2023-05-10 23:42:53 +0200

    Update eyesonrussia.py

--- eyesonrussia.py
+++ eyesonrussia.py
@@ -6,6 +6,8 @@ import requests
 import json
 import time
 import argparse
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 
 parser = argparse.ArgumentParser(description='Generates a JSON file with events data from a URL')
 parser.add_argument('--city', type=str, help='Filter by city')
@@ -13,11 +15,19 @@ parser.add_argument('--date', type=str, help='Filter by date')
 parser.add_argument('--country', type=str, help='Filter by country')
 parser.add_argument('--category', type=str, help='Filter by category')
 parser.add_argument('--latlon', type=str, help='Filter events by latitude and longitude (lat,lon)')
-parser.add_argument('--format', type=str, choices=['json', 'excel'], default='json', help='Output file format (json or excel)')
+parser.add_argument('--format', type=str, choices=['json', 'xlsx'], default='json', help='Output file format (json or excel)')
+parser.add_argument('--backup', type=str, help='optional argument, type yes to save a xlsx into your Google Drive account') # If you add this argument, make sure you have also completed creds.json
+
 args = parser.parse_args()
 
-
 start_time = time.time()
+
+# small function to display the status while executing the script
+def run_indicator(num_iterations):
+    for i in range(num_iterations):
+        print("[*] Processing data" + "." * (i+1), end="\r")
+        time.sleep(0.05)
+    print("[*] Data uploaded successfully.")
 
 # block for the request
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:79.0) Gecko/20100101 Firefox/79.0', 'Accept':'*/*','Content-Type':'application/json'}  
@@ -80,15 +90,39 @@ if args.latlon:
 
 # Generate output file with filtered events
 if args.format == 'json':
-    file_name = f"UWMaphub_events_filtered{'_' + args.city if args.city else ''}{'_' + args.date if args.date else ''}{'_' + args.country if args.country else ''}{'_' + args.category if args.category else ''}{'_' + args.latlon if args.latlon else ''}.json"
+    file_name = f"EOR_events_filtered{'_' + args.city if args.city else ''}{'_' + args.date if args.date else ''}{'_' + args.country if args.country else ''}{'_' + args.category if args.category else ''}{'_' + args.latlon if args.latlon else ''}.json"
     with open(file_name, 'w', encoding='utf-8') as file:
         json.dump(events, file, ensure_ascii=False, indent=4)
     print("[*] Operation has finished succesfully")
-    print("[*]", len(events), f'events deta have been added to the file "UWMaphub_events.json" in {str(elapsed_time)[0:4]} seconds')
+    print("[*]", len(events), f'events have been added to the file "EOR_events.json" in {str(elapsed_time)[0:4]} seconds')
     
 elif args.format == 'excel':
-    file_name = f"UWMaphub_events_filtered{'_' + args.city if args.city else ''}{'_' + args.date if args.date else ''}{'_' + args.country if args.country else ''}{'_' + args.category if args.category else ''}{'_' + args.latlon if args.latlon else ''}.xlsx"
+    file_name = f"EOR_events_filtered{'_' + args.city if args.city else ''}{'_' + args.date if args.date else ''}{'_' + args.country if args.country else ''}{'_' + args.category if args.category else ''}{'_' + args.latlon if args.latlon else ''}.xlsx"
     df = pd.DataFrame(events)
     df.to_excel(file_name, index=False)
     print("[*] Operation has finished succesfully")
-    print("[*]", len(events), f'events have been added to the file "UWMaphub_events.xlsx" in {str(elapsed_time)[0:4]} seconds')
+    print("[*]", len(events), f'events have been added to the file "EOR_events.xlsx" in {str(elapsed_time)[0:4]} seconds')
+
+#     
+if args.backup:
+    print("[*] Processing... wait a moment please. Each event is being sent after 1 second to avoid burn out your quota")
+    scope = ['https://www.googleapis.com/auth/drive', 'https://www.googleapis.com/auth/spreadsheets']
+    creds = ServiceAccountCredentials.from_json_keyfile_name('creds.json', scope)
+    client = gspread.authorize(creds)
+    new_sheet = client.create(f"EOR_events_filtered{'_' + args.city if args.city else ''}{'_' + args.date if args.date else ''}{'_' + args.country if args.country else ''}{'_' + args.category if args.category else ''}{'_' + args.latlon if args.latlon else ''}")
+    # ATTENTION, ADD YOUR CLIENT SERVICE EMAIL INTO THE NEXT LINE
+    client.insert_permission(new_sheet.id, 'INTRODUCE HERE YOUR CLIENT SERVICE EMAIL', perm_type='user', role='writer')
+    # ATTENTION, ADD YOUR DESTINATION EMAIL INTO THE NEXT LINE
+    client.insert_permission(new_sheet.id, 'INTRODUCE HERE YOUR EMAIL DESTINATARY', perm_type='user', role='writer') 
+    sheet = client.open(f"EOR_events_filtered{'_' + args.city if args.city else ''}{'_' + args.date if args.date else ''}{'_' + args.country if args.country else ''}{'_' + args.category if args.category else ''}{'_' + args.latlon if args.latlon else ''}").sheet1
+    sheet_headers = ['EVENT ID', 'DATE', 'DISTRICT', 'CITY', 'PROVINCE', 'COUNTRY', 'LATITUDE', 'LONGITUDE', 'SOURCE', 'STATUS', 'CREDIT', 'CATEGORIES']
+    sheet.insert_row(sheet_headers)
+    try:
+        for event in events:
+            run_indicator(len(events))
+            time.sleep(1)
+            categories = ', '.join(event['categories'])
+            sheet.append_row([event['id'], event['date'], event['district'], event['city'], event['province'], event['country'], event['latitude'], event['longitude'], event['source'], event['status'], event['credit'], categories])
+        print("[*] Data has been successfully backed up in your as a spreadsheet in your Google Drive account")
+    except gspread.exceptions.APIError as error:
+        print("[*] Sorry, there was an error. More details here:", error)
